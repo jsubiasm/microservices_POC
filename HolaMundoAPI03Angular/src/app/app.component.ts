b5dbe88b073f70a1889029591facbe1da7e29434
jsubiasm <jsubiasm@yahoo.es>
2017-07-14 08:30:56 +0200

    Importada cliente API from swaggergen

--- HolaMundoAPI03Angular/src/app/app.component.ts
+++ HolaMundoAPI03Angular/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { DefaultApi } from '../api/DefaultApi';
+import { OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+@Component( {
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    providers: [DefaultApi]
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+    constructor( private api: DefaultApi ) { }
+
+    title: string;
+
+    ngOnInit(): void {
+
+        this.api.holamundoGet().subscribe(res => {this.title = res; });
+        console.log('HolaMundoAPI [' +  this.title  + ']');
+
+    }
+
 }
