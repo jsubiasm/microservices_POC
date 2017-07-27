46ed240a6e6a1d8fae3eebcd8a329acf7201ced9
jsubiasm <jsubiasm@yahoo.es>
2017-07-27 11:48:17 +0200

    Commit inicial del proyecto Spring Boot generado con eclipse.
    Definiciones del API en formato JSON y YAML.

---
+++ PersonasApiRestful/src/test/java/jsm/personasapirestful/PersonasApiRestfulApplicationTests.java
@@ -0,0 +1,16 @@
+package jsm.personasapirestful;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PersonasApiRestfulApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
