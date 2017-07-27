5194251cfc947f50da0afb8fb0b974e02c479f28
jsubiasm <jsubiasm@yahoo.es>
2017-07-27 13:00:45 +0200

    Commit inicial del proyecto segun fue generado con eclipse

---
+++ PersonaAPIAngular/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { PersonaAPIAngularPage } from './app.po';
+
+describe('PersonaAPIAngular App', function() {
+  let page: PersonaAPIAngularPage;
+
+  beforeEach(() => {
+    page = new PersonaAPIAngularPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
