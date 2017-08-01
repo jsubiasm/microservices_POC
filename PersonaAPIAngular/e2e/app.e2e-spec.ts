666f61dec8ff7b8c9c2bd21207b706df6b517207
jsubiasm <jsubiasm@yahoo.es>
2017-08-01 11:53:59 +0200

    Arreglados los tests

--- PersonaAPIAngular/e2e/app.e2e-spec.ts
+++ PersonaAPIAngular/e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('PersonaAPIAngular App', function() {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Prueba Concepto API RESTful');
   });
 });
--- PersonaAPIAngular/src/app/app.component.spec.ts
+++ PersonaAPIAngular/src/app/app.component.spec.ts
@@ -2,12 +2,22 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { AddPersonaComponent } from './add-persona/add-persona.component';
+import { UpdatePersonaComponent } from './update-persona/update-persona.component';
+import { FindPersonaComponent } from './find-persona/find-persona.component';
+import { DeletePersonaComponent } from './delete-persona/delete-persona.component';
+import { ListPersonasComponent } from './list-personas/list-personas.component';
 
 describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        AddPersonaComponent,
+        UpdatePersonaComponent,
+        FindPersonaComponent,
+        DeletePersonaComponent,
+        ListPersonasComponent        
       ],
     });
     TestBed.compileComponents();
@@ -19,16 +29,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have as title 'Prueba Concepto API RESTful'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toEqual('Prueba Concepto API RESTful');
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h4').textContent).toContain('Prueba Concepto API RESTful');
   }));
 });
