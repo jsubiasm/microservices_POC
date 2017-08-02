d9b11f0585932a05bb8b1a86134daeabc45ccc7e
jsubiasm <jsubiasm@yahoo.es>
2017-08-02 12:30:24 +0200

    Formulario angularizado

--- PersonaAPIAngular/src/app/add-persona/add-persona.component.ts
+++ PersonaAPIAngular/src/app/add-persona/add-persona.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-add-persona',
-  templateUrl: './add-persona.component.html',
-  styleUrls: ['./add-persona.component.css']
+import { AddPersonaBody } from '../../model/AddPersonaBody';
+
+@Component( {
+    selector: 'app-add-persona',
+    templateUrl: './add-persona.component.html',
+    styleUrls: ['./add-persona.component.css']
 })
 export class AddPersonaComponent implements OnInit {
 
-  constructor() { }
+    persona: AddPersonaBody;
+    resultOK: boolean;
+    resultERR: boolean;
+    resultadoMSG: String;
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    focus() {
+        this.resultOK = false;
+        this.resultERR = false;
+        this.resultadoMSG = "";
+    }
+
+    init() {
+        this.persona = { id: null, nombre: "", profesion: "" };
+        this.focus();
+    }
+
+    ngOnInit() {
+        this.init();
+    }
+
+    onSubmit() {
+        try {
+            this.resultOK = true;
+            this.resultERR = false;
+            this.resultadoMSG = "Persona insertada [" + this.persona.nombre + "] [" + this.persona.profesion + "]";
+        }
+        catch ( error ) {
+            this.resultOK = false;
+            this.resultERR = true;
+            this.resultadoMSG = "Error [" + error.message + "]";
+        }
+    }
 
 }
