fe2dd82305d02a4bd57cd0be542372200a43ff5b
jsubiasm <jsubiasm@yahoo.es>
2017-08-02 08:42:46 +0200

    Test update

--- PersonaAPIAngular/src/app/app.component.spec.ts
+++ PersonaAPIAngular/src/app/app.component.spec.ts
@@ -35,7 +35,7 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('Prueba Concepto API RESTful');
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should render title in a h4 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
