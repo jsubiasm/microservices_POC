8edce38dd969330d2cf77c31954655e60b1352c2
jsubiasm <jsubiasm@yahoo.es>
2017-08-03 13:06:43 +0200

    Archivos CMD

---
+++ PersonaAPIBoot/run.cmd
@@ -0,0 +1,9 @@
+@echo off
+
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_91
+set M2_HOME=C:\_PELAYO\Software\Maven\apache-maven-3.3.9
+PATH=%JAVA_HOME%\bin;%M2_HOME%\bin
+
+java -jar .\target\PersonaAPIBoot-1.0.0.jar
+
+pause
