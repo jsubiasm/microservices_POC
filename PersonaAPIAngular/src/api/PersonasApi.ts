b41d14262bf7274437baf6809ce25fb265f368d1
jsubiasm <jsubiasm@yahoo.es>
2017-08-11 12:32:49 +0200

    Arrancamos spring boot en el puerto 85 en local y en docker

--- PersonaAPIAngular/src/api/PersonasApi.ts
+++ PersonaAPIAngular/src/api/PersonasApi.ts
@@ -28,7 +28,7 @@ import { Configuration }                                     from '../configurat
 @Injectable()
 export class PersonasApi {
 
-    protected basePath = 'http://localhost:8080/v1';
+    protected basePath = 'http://localhost:85/v1';
     public defaultHeaders: Headers = new Headers();
     public configuration: Configuration = new Configuration();
 
