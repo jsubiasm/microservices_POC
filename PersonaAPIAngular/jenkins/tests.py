3c040d83fa2905adc445a0947e80119e82fedf5d
jsubiasm <jsubiasm@yahoo.es>
2017-08-11 08:49:27 +0200

    Modificado script

--- PersonaAPIAngular/jenkins/tests.py
+++ PersonaAPIAngular/jenkins/tests.py
@@ -1,6 +1,19 @@
+import subprocess
+
 print("*********************************************")
 print("*                                           *")
 print("*  WARNING: Es necesario ejecutar los tests *")
 print("*           unitarios de angular            *")
 print("*                                           *")
 print("*********************************************")
+
+print("Versiones de software:")
+
+subprocess.call(["java", "-version"])
+subprocess.call(["python", "-V"])
+subprocess.call(["python3", "-V"])
+subprocess.call(["mvn", "-version"])
+subprocess.call(["docker", "-v"])
+subprocess.call(["node", "-v"])
+subprocess.call(["npm", "-v"])
+subprocess.call(["ng", "-v"])
