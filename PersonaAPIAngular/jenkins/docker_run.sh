14176fc80da9fed3bd03a95cc664a804af5b59a5
jsubiasm <jsubiasm@yahoo.es>
2017-09-04 15:45:29 +0200

    script jenkins docker run

---
+++ PersonaAPIAngular/jenkins/docker_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd ..
+sudo npm install
+sudo ng build
+sudo cp -r ./dist ./docker
+cd ./docker
+sudo docker build -t jsubiasm/persona-api-angular .
+sudo docker stop persona-api-angular
+sudo docker rm persona-api-angular
+sudo docker run -d --restart unless-stopped --name persona-api-angular -p 86:80 -t jsubiasm/persona-api-angular
