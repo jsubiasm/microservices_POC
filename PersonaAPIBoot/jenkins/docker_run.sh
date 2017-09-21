21dea0a7e7c22e8321eae697f0223241a831cf0d
jsubiasm <jsubiasm@yahoo.es>
2017-09-21 17:56:40 +0200

    Escalamos el API a dos contenedores con puertos aleatorios registrados en eureka (sin docker-compose)

--- PersonaAPIBoot/jenkins/docker_run.sh
+++ PersonaAPIBoot/jenkins/docker_run.sh
@@ -1,10 +1,13 @@
 echo '--- INICIO docker_run.sh ---'
 cd ./PersonaAPIBoot
 sudo cp ./target/PersonaAPIBoot-1.0.0.jar ./docker/PersonaAPIBoot-1.0.0.jar
-sudo docker stop persona-api-boot
-sudo docker rm persona-api-boot
+sudo docker stop persona-api-boot-01
+sudo docker stop persona-api-boot-02
+sudo docker rm persona-api-boot-01
+sudo docker rm persona-api-boot-02
 sudo docker image rm jsubiasm/persona-api-boot
 cd ./docker
 sudo docker build -t jsubiasm/persona-api-boot .
-sudo docker run -d --restart unless-stopped --name persona-api-boot -P --network="host" -t jsubiasm/persona-api-boot
+sudo docker run -d --restart unless-stopped --name persona-api-boot-01 -P --network="host" -t jsubiasm/persona-api-boot
+sudo docker run -d --restart unless-stopped --name persona-api-boot-02 -P --network="host" -t jsubiasm/persona-api-boot
 echo '--- FIN docker_run.sh ---'
