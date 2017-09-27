6749591b30e4235cec7868b953ce3cf378313553
jsubiasm <jsubiasm@yahoo.es>
2017-09-27 13:00:00 +0200

    Añadido sleuth sin zipkin en todos los proyectos (menos zuul y api)

--- NetflixHystrix/src/main/java/jsm/mspoc/netflix/hystrix/NetflixHystrixApplication.java
+++ NetflixHystrix/src/main/java/jsm/mspoc/netflix/hystrix/NetflixHystrixApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 
 @EnableEurekaClient
 @EnableHystrixDashboard
@@ -17,4 +19,11 @@ public class NetflixHystrixApplication
 	{
 		SpringApplication.run(NetflixHystrixApplication.class, args);
 	}
+
+	@Bean
+	public AlwaysSampler defaultSampler()
+	{
+		return new AlwaysSampler();
+	}
+
 }
--- ZipkinServer/src/main/java/jsm/mspoc/zipkin/ZipkinServerApplication.java
+++ ZipkinServer/src/main/java/jsm/mspoc/zipkin/ZipkinServerApplication.java
@@ -2,6 +2,8 @@ package jsm.mspoc.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 
 import zipkin.server.EnableZipkinServer;
 
@@ -14,4 +16,11 @@ public class ZipkinServerApplication
 	{
 		SpringApplication.run(ZipkinServerApplication.class, args);
 	}
+
+	@Bean
+	public AlwaysSampler defaultSampler()
+	{
+		return new AlwaysSampler();
+	}
+
 }
